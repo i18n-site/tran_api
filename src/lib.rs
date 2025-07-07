aac248866b2bcb15c026adcb3f22e6ea99c0287b
i18n <i18n.site@gmail.com>
2025-07-08 05:59:39 +0800

    ♨

--- src/lib.rs
+++ src/lib.rs
@@ -1,1 +1,19 @@
 tonic::include_proto!("api");
+
+use bytes::Bytes;
+use grpc_client::endpoint;
+use tonic::{
+  codec::CompressionEncoding,
+  transport::{Channel, Error},
+};
+
+use crate::api_client::ApiClient;
+
+pub async fn conn(addr: impl Into<Bytes>) -> Result<ApiClient<Channel>, Error> {
+  Ok(
+    ApiClient::connect(endpoint(addr)?)
+      .await?
+      .send_compressed(CompressionEncoding::Gzip)
+      .accept_compressed(CompressionEncoding::Gzip),
+  )
+}
