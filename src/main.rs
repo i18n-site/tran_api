fdd2db06e514cfa0a94515bad9ceb1f545d1e197
i18n <i18n.site@gmail.com>
2025-07-08 06:37:53 +0800

    ♨

--- src/main.rs
+++ src/main.rs
@@ -1,4 +1,4 @@
-genv::s!(I18N_GRPC: String | "http://127.0.0.1:3333/".to_owned());
+genv::s!(I18N_GRPC: String | "http://127.0.0.1:3333".to_owned());
 
 #[tokio::main]
 async fn main() -> Result<(), Box<dyn std::error::Error>> {
