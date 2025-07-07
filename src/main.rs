5a08b0552070712610560f9cb1e12fa7c27292b6
i18n <i18n.site@gmail.com>
2025-07-08 06:07:10 +0800

    ♨

--- src/main.rs
+++ src/main.rs
@@ -2,7 +2,8 @@ genv::s!(I18N_GRPC: String | "http://127.0.0.1:3333/".to_owned());
 
 #[tokio::main]
 async fn main() -> Result<(), Box<dyn std::error::Error>> {
-  let api = tran_api::conn(I18N_GRPC.as_str()).await?;
-
+  let mut api = tran_api::conn(I18N_GRPC.as_str()).await?;
+  let version = api.version(tran_api::Void {}).await?.into_inner();
+  println!("v{}.{}.{}", version.major, version.minor, version.patch);
   Ok(())
 }
