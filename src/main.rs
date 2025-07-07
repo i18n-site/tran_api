7e06752ae207c441354cbfc0de73ec7af7255720
i18n <i18n.site@gmail.com>
2025-07-08 06:04:20 +0800

    ♨

--- src/main.rs
+++ src/main.rs
@@ -1,4 +1,7 @@
+genv::s!(I18N_GRPC: String | "http://127.0.0.1:3333/".to_owned());
+
 #[tokio::main]
-async fn main() {
-  println!("Hello, world!");
+async fn main() -> Result<(), Box<dyn std::error::Error>> {
+  let api = tran_api::conn(I18N_GRPC.as_str()).await?;
+  Ok(())
 }
