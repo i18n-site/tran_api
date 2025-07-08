418eae2200bb148c5b3347f8d93401757cf72831
i18n <i18n.site@gmail.com>
2025-07-08 21:13:43 +0800

    ♨

--- src/lib.rs
+++ src/lib.rs
@@ -1,10 +1,10 @@
 tonic::include_proto!("api");
 
 use bytes::Bytes;
-use grpc_client::endpoint;
+use http::HeaderValue;
 use tonic::{
   Request, Status,
-  transport::{Channel, Error},
+  transport::{Channel, Endpoint, Error},
 };
 
 use crate::api_client::ApiClient;
@@ -14,9 +14,14 @@ pub type ApiClientWithHeader =
   ApiClient<tonic::service::interceptor::InterceptedService<Channel, Interceptor>>;
 
 pub async fn conn(addr: impl Into<Bytes>) -> Result<ApiClient<Channel>, Error> {
-  let channel = endpoint(addr)?;
+  let endpoint = Endpoint::from_shared(addr.into())?.user_agent(
+    HeaderValue::from_bytes(
+      const_str::concat!(env!("CARGO_PKG_NAME"), "/", env!("CARGO_PKG_VERSION")).as_bytes(),
+    )
+    .unwrap(),
+  )?;
 
-  let client = ApiClient::connect(channel).await?;
+  let client = ApiClient::connect(endpoint).await?;
 
   Ok(client)
 }
