e11e38eba6440b41644de47aeb18f6452c357e27
i18n <i18n.site@gmail.com>
2025-07-08 21:01:26 +0800

    ♨

--- src/lib.rs
+++ src/lib.rs
@@ -1,4 +1,5 @@
 tonic::include_proto!("api");
+
 use bytes::Bytes;
 use grpc_client::endpoint;
 use tonic::{
@@ -8,26 +9,14 @@ use tonic::{
 
 use crate::api_client::ApiClient;
 
-#[allow(clippy::result_large_err)]
-fn req_interceptor(mut req: Request<()>) -> Result<Request<()>, Status> {
-  let meta = req.metadata_mut();
-  // meta.remove("user-agent");
-  meta.insert(
-    "t",
-    tonic::metadata::AsciiMetadataValue::from_key::<tonic::metadata::Ascii>(
-      "11xvw".parse().unwrap(),
-    ),
-  );
-  Ok(req)
-}
 type Interceptor = fn(Request<()>) -> Result<Request<()>, Status>;
 pub type ApiClientWithHeader =
   ApiClient<tonic::service::interceptor::InterceptedService<Channel, Interceptor>>;
 
 pub async fn conn(addr: impl Into<Bytes>) -> Result<ApiClientWithHeader, Error> {
-  let channel = endpoint(addr)?.connect().await?;
+  let channel = endpoint(addr)?;
 
-  let client = ApiClient::with_interceptor(channel, req_interceptor as Interceptor);
+  let client = ApiClient::connect(channel).await?;
 
   Ok(client)
 }
