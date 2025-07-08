0b94e04ca45640ae30f1a25277e809e0f14f907f
i18n <i18n.site@gmail.com>
2025-07-08 20:16:09 +0800

    ♨

--- src/lib.rs
+++ src/lib.rs
@@ -1,19 +1,31 @@
 tonic::include_proto!("api");
-
+use CompressionEncoding::Gzip;
 use bytes::Bytes;
 use grpc_client::endpoint;
 use tonic::{
+  Request, Response,
+  body::Body,
+  client::GrpcService,
   codec::CompressionEncoding,
+  service::{Interceptor, interceptor::InterceptedService},
   transport::{Channel, Error},
 };
 
 use crate::api_client::ApiClient;
 
+pub fn req_interceptor(mut req: tonic::Request<()>) -> tonic::Result<tonic::Request<()>> {
+  req
+    .metadata_mut()
+    .insert("Authorization", "XXXXXXXXXXXX".try_into().unwrap());
+  Ok(req)
+}
 pub async fn conn(addr: impl Into<Bytes>) -> Result<ApiClient<Channel>, Error> {
-  Ok(
-    ApiClient::connect(endpoint(addr)?)
-      .await?
-      .send_compressed(CompressionEncoding::Gzip)
-      .accept_compressed(CompressionEncoding::Gzip),
-  )
+  let channel = endpoint(addr)?;
+
+  let client = ApiClient::connect(channel)
+    .await?
+    .send_compressed(Gzip)
+    .accept_compressed(Gzip);
+
+  Ok(client)
 }
