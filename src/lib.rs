968c3dd9f455ca369e090681d028f8c97ea3a3c6
i18n <i18n.site@gmail.com>
2025-07-08 21:14:04 +0800

    ♨

--- src/lib.rs
+++ src/lib.rs
@@ -2,16 +2,9 @@ tonic::include_proto!("api");
 
 use bytes::Bytes;
 use http::HeaderValue;
-use tonic::{
-  Request, Status,
-  transport::{Channel, Endpoint, Error},
-};
+use tonic::transport::{Channel, Endpoint, Error};
 
 use crate::api_client::ApiClient;
-
-type Interceptor = fn(Request<()>) -> Result<Request<()>, Status>;
-pub type ApiClientWithHeader =
-  ApiClient<tonic::service::interceptor::InterceptedService<Channel, Interceptor>>;
 
 pub async fn conn(addr: impl Into<Bytes>) -> Result<ApiClient<Channel>, Error> {
   let endpoint = Endpoint::from_shared(addr.into())?.user_agent(
