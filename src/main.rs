e5adb9b68394ec05c798470966044c7215371c8b
i18n <i18n.site@gmail.com>
2025-07-09 13:54:18 +0800

    .

--- src/main.rs
+++ src/main.rs
@@ -8,6 +8,6 @@ async fn main() -> Result<(), Box<dyn std::error::Error>> {
     let mut api = tran_api::conn(I18N_GRPC.as_str(), Ok).await?;
     let version = api.version(tran_api::Void {}).await?.into_inner();
     println!("v{}.{}.{}", version.major, version.minor, version.patch);
-    Ok(())
   }
+  Ok(())
 }
