0d1be2c24aa64489ce1f0fc86f6e74483b5d869b
i18n <i18n.site@gmail.com>
2025-07-09 11:12:57 +0800

    ♨

--- src/lib.rs
+++ src/lib.rs
@@ -1,22 +1,24 @@
 tonic::include_proto!("api");
 
 use bytes::Bytes;
-use tonic::service::Interceptor;
-use tonic::transport::{Channel, Endpoint, Error};
+use tonic::{
+  service::Interceptor,
+  transport::{Channel, Endpoint, Error},
+};
 
 use crate::api_client::ApiClient;
 
 pub async fn conn<F>(
-    addr: impl Into<Bytes>,
-    interceptor: F,
+  addr: impl Into<Bytes>,
+  interceptor: F,
 ) -> Result<ApiClient<tonic::service::interceptor::InterceptedService<Channel, F>>, Error>
 where
-    F: Interceptor,
+  F: Interceptor,
 {
-    let endpoint = Endpoint::from_shared(addr.into())?;
-    let channel = endpoint.connect().await?;
+  let endpoint = Endpoint::from_shared(addr.into())?;
+  let channel = endpoint.connect().await?;
 
-    let client = ApiClient::with_interceptor(channel, interceptor);
+  let client = ApiClient::with_interceptor(channel, interceptor);
 
-    Ok(client)
+  Ok(client)
 }
