d3a244356a73a972213d38408d54dfd311293edd
i18n <i18n.site@gmail.com>
2025-07-09 13:54:00 +0800

    .

--- src/main.rs
+++ src/main.rs
@@ -1,11 +1,13 @@
 #[cfg(feature = "bin")]
 genv::s!(I18N_GRPC: String | "http://127.0.0.1:3333".to_owned());
 
-#[cfg(feature = "bin")]
 #[tokio::main]
 async fn main() -> Result<(), Box<dyn std::error::Error>> {
-  let mut api = tran_api::conn(I18N_GRPC.as_str(), Ok).await?;
-  let version = api.version(tran_api::Void {}).await?.into_inner();
-  println!("v{}.{}.{}", version.major, version.minor, version.patch);
-  Ok(())
+  #[cfg(feature = "bin")]
+  {
+    let mut api = tran_api::conn(I18N_GRPC.as_str(), Ok).await?;
+    let version = api.version(tran_api::Void {}).await?.into_inner();
+    println!("v{}.{}.{}", version.major, version.minor, version.patch);
+    Ok(())
+  }
 }
